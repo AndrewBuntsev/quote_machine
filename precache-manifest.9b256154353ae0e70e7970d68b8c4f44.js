9b002e2c6621169b12a4fb0328e7f2c5a6d9b6bd
ABuntsev <andrew.buntsev@gmail.com>
2019-03-23 14:31:02 +1100

    Updates

---
+++ precache-manifest.9b256154353ae0e70e7970d68b8c4f44.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/quote_machine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "51ee7c3bcce8d021ce4a",
+    "url": "/quote_machine/static/css/main.dfd237eb.chunk.css"
+  },
+  {
+    "revision": "69dab1035e8aa983d83f",
+    "url": "/quote_machine/static/js/runtime~main.6b12df4c.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/quote_machine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "cdb8fb842a267bb7fe55",
+    "url": "/quote_machine/static/js/2.fe28cdb7.chunk.js"
+  },
+  {
+    "revision": "51ee7c3bcce8d021ce4a",
+    "url": "/quote_machine/static/js/main.94c950e3.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/quote_machine/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/quote_machine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/quote_machine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "cdb8fb842a267bb7fe55",
+    "url": "/quote_machine/static/css/2.8766bdf0.chunk.css"
+  },
+  {
+    "revision": "52742fcf21ca5c5865037f3ba2a66e03",
+    "url": "/quote_machine/index.html"
+  }
+];
